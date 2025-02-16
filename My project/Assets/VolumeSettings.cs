36f2ebf2c5bae97a997b7950bba29a199516484d
ThrilochanaSivarajah <slochana03@gmail.com>
2025-02-17 01:12:38 +0530

    Add sound UI

---
+++ My project/Assets/VolumeSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioSource myMixer;
+    [SerializeField] private Slider musicSlider;
+}
