4f1ffecfb91650afdba78d949ea371e51922a5a8
Kaja <kajadeeban@gmail.com>
2025-02-18 22:57:54 +0530

    Update: Added Main Menu with Volume settings

--- Kaja-EQ/EduQuest/Assets/Scripts/AudioManager.cs
+++ Kaja-EQ/EduQuest/Assets/Scripts/AudioManager.cs
@@ -27,7 +27,8 @@ public class AudioManager : MonoBehaviour {
 			s.source.volume = s.volume;
 			s.source.pitch = s.pitch;
 			s.source.loop = s.loop;
-		}
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
 	}
 
 	public void Play(string sound)
