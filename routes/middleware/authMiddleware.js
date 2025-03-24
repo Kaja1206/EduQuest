b77ac26c1e9d10edd2636e48469e56d3db2d3dc4
Thivyadeekzan Manievannan <thivyadeekzanmanievannan@gmail.com>
2025-03-24 10:17:44 +0530

    Connecting the ends

--- routes/middleware/authMiddleware.js
+++ routes/middleware/authMiddleware.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-    const token = req.cookies.token; // get tokens from cookies
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const token = req.cookies.token; // get tokens from cookies
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 };
 
 module.exports = authenticateUser;
